055bfa24f05e5b6e118ddbcd0f18bf6d44038260
Egor Belov <kochetov_zhora@mail.ru>
2025-10-19 17:20:01 +0300

    upgraded homework

--- src/processing.py
+++ src/processing.py
@@ -1,19 +1,24 @@
+from datetime import datetime
+
+
 def filter_by_state(my_list: list, state: str = "EXECUTED") -> list:
+    """
+       Функция фильтрует список словарей по ключу 'state'.
+       :param my_list: список словарей, содержащих ключ 'state'
+       :param state: статус фильтрации
+       :return: отфильтрованный список
+    """
     result = list(filter(lambda item: item.get("state") == state, my_list))
     return result
-"""Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state 
-соответствует указанному значению"""
-
-from datetime import datetime
 
 
 def sort_by_date(data, ascending=False):
     """
-    Сортирует список словарей по ключу 'date'.
+        Сортирует список словарей по ключу 'date'.
 
-    :param data: список словарей, содержащих ключ 'date'
-    :param ascending: порядок сортировки (по возрастанию, если True; по убыванию — по умолчанию)
-    :return: новый отсортированный список
+        :param data: список словарей, содержащих ключ 'date'
+        :param ascending: порядок сортировки (по возрастанию, если True; по убыванию — по умолчанию)
+        :return: новый отсортированный список
     """
     return sorted(
         data,
