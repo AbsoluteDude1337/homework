2483ce491627533d28b279857078da2dc76805c6
Egor Belov <kochetov_zhora@mail.ru>
2025-10-26 16:04:38 +0300

    tests create 3.0

--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,8 +1,12 @@
 from src.widget import get_date, mask_account_card
 
 def test_get_date():
-    assert get_date('2004-02-01') == "01.02.2004"
+    assert get_date('2005-02-01') == "01.02.2005"
 
 
 def test_mask_account_card():
-    assert mask_account_card('1234567810111213') == '1234 56** **** 1213'
+    assert mask_account_card('1234567810111313') == '1234 56** **** 1313'
+
+
+def test_mask_account_card_2():
+    assert mask_account_card('Счёт 1234567810111313') == '1234 56** **** 1313'
