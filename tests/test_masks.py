46a445c3cf90f554c22f03376659e81bef7bfa6e
Egor Belov <kochetov_zhora@mail.ru>
2025-10-26 16:39:19 +0300

    Были добавлены фикстуры и параметризация

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,12 +1,19 @@
 from src.masks import get_mask_card_number
+import pytest
+@pytest.fixture
+def zalupa():
+    return 1234567810111231
 
-
-def test_get_mask_card_number():
-    assert get_mask_card_number(1234567810111231) == '1234 56** **** 1231'
+def test_get_mask_card_number(zalupa):
+    data = zalupa
+    assert get_mask_card_number(data) == '1234 56** **** 1231'
 
 
 from src.masks import get_mask_account
 
-
-def test_get_mask_account():
-    assert get_mask_account(12345678910111213141) == '**3141'
+@pytest.fixture
+def zalupa_1():
+    return 12345678910111213141
+def test_get_mask_account(zalupa_1):
+    data = zalupa_1
+    assert get_mask_account(data) == '**3141'
