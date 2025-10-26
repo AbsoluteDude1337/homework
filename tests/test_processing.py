13810b1658931a73a153da072991850351ab4cd7
Egor Belov <kochetov_zhora@mail.ru>
2025-10-26 15:46:38 +0300

    tests create 3.0

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -2,11 +2,11 @@ from src.processing import filter_by_state
 
 
 def test_filter_by_state():
-    assert filter_by_state() == ''
+    assert filter_by_state([{}]) == []
 
 
 from src.processing import sort_by_date
 
 
 def test_sort_by_date():
-    assert sort_by_date() == ""
+    assert sort_by_date([{'date':'2002-10-10'}]) == [{'date':'2002-10-10'}]
